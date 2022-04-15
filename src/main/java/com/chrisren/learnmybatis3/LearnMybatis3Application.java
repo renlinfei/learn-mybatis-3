70d0ff6786f435311c1f08ff9eace509ff49ad30
Chris Ren <chrisren@192.168.1.6>
2022-04-15 22:14:18 +0800

    Initial commit

---
+++ src/main/java/com/chrisren/learnmybatis3/LearnMybatis3Application.java
@@ -0,0 +1,13 @@
+package com.chrisren.learnmybatis3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMybatis3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMybatis3Application.class, args);
+    }
+
+}
